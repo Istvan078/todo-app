0c2635bbdb1614b5b0e4812583f52bde46316a99
Istvan078 <kalmaristvan078@gmail.com>
2026-02-19 22:17:30 +0000

    Notif Subscription test

--- todo-react/src/push/pushSettings.tsx
+++ todo-react/src/push/pushSettings.tsx
@@ -1,9 +1,13 @@
+import { useCreateSub } from "@/hooks/useCreateSub.hook";
 import { useFetchPubKey } from "@/hooks/useFetchPubKey.hook";
+import type { IPushSubscribeBody } from "@/types/pushNotification.interface";
 // import { useQueryClient } from "@tanstack/react-query";
 import type { ReactElement } from "react";
 
 export function PushSettings(): ReactElement {
   const { data, isLoading, error } = useFetchPubKey();
+  const { mutate } = useCreateSub();
+
   const subForPushNotif = () => {
     // itt majd a subscribe logika lesz (permission + pushManager.subscribe + POST /subscribe)
     console.log(data?.data?.publicKey.publicKey);
@@ -22,9 +26,12 @@ export function PushSettings(): ReactElement {
       userVisibleOnly: true,
       applicationServerKey: publicKey,
     });
-    const body = subscription.toJSON();
-    console.log(body);
+    const subBody = subscription.toJSON() as unknown as IPushSubscribeBody;
+    mutate(subBody, {
+      onSuccess: (data) => console.log(data),
+    });
   }
+
   return (
     <div>
       <button onClick={subForPushNotif}>Subscribe</button>
