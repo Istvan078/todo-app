15d825b4f85710108d321ca387d3bdda01b4aec9
Istvan078 <kalmaristvan078@gmail.com>
2026-03-10 17:06:02 +0000

    Added avatar loader

--- todo-react/src/components/taskSidebar/taskSidebar.tsx
+++ todo-react/src/components/taskSidebar/taskSidebar.tsx
@@ -1,4 +1,4 @@
-import type { FC, ReactElement } from "react";
+import { useState, type FC, type ReactElement } from "react";
 import { Card } from "@/components/ui/card";
 import { UserProfile } from "../userProfile/userProfile";
 import { CreateTaskForm } from "../createTaskForm/createTaskForm";
@@ -6,6 +6,7 @@ import type { ITask } from "@/types/task.interface";
 import { XIcon } from "lucide-react";
 import { useUpdateUser } from "@/hooks/useUpdateUser.hook";
 import { useQueryClient } from "@tanstack/react-query";
+import { Spinner } from "@/components/ui/spinner";
 
 type TaskSidebarProps = {
   onClose: () => void;
@@ -28,12 +29,14 @@ export const TaskSidebar: FC<TaskSidebarProps> = ({
 }: TaskSidebarProps): ReactElement => {
   const { mutate: updateUser } = useUpdateUser();
   const queryClient = useQueryClient();
+  const [isUpdatingAvatar, setIsUpdatingAvatar] = useState(false);
 
   function handleUpdateUser(file: File) {
     const formData = new FormData();
-    if(file)
-    formData.append("avatar", file);
-    if(user.avatarPublicId) formData.append("avatarPublicId", user.avatarPublicId);
+    if (file) formData.append("avatar", file);
+    if (user.avatarPublicId)
+      formData.append("avatarPublicId", user.avatarPublicId);
+    setIsUpdatingAvatar(true);
     updateUser(formData, {
       onSuccess: (response) => {
         // Update the token in localStorage with the new token from the response
@@ -43,9 +46,11 @@ export const TaskSidebar: FC<TaskSidebarProps> = ({
           queryKey: ["fetchTasks"],
           refetchType: "all",
         });
+        setIsUpdatingAvatar(false);
       },
       onError: (error) => {
         console.error("Failed to update user:", error);
+        setIsUpdatingAvatar(false);
       },
     });
   }
@@ -60,10 +65,18 @@ export const TaskSidebar: FC<TaskSidebarProps> = ({
             onClick={onClose}
           ></XIcon>
         )}
-        <UserProfile
-          onAvatarChange={handleUpdateUser}
-          user={user}
-        ></UserProfile>
+        {isUpdatingAvatar && (
+          <div>
+            <Spinner className="h-8 w-8 absolute top-15 left-1/2 transform -translate-x-1/2"></Spinner>
+          </div>
+        )}
+        {!isUpdatingAvatar && (
+          <UserProfile
+            onAvatarChange={handleUpdateUser}
+            user={user}
+          ></UserProfile>
+        )}
+
         <CreateTaskForm
           editTaskData={editTaskData}
           onCreated={onClose}
